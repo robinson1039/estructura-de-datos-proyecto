8b442b21b127e913584140f0574a401ec0d155f5
Shara García <shara.garcia.betancur@gmail.com>
2025-05-29 02:52:04 -0500

    Organizar las rutas

--- back/src/routes/pc_router.py
+++ back/src/routes/pc_router.py
@@ -3,9 +3,11 @@ from controllers.pcsController import create_pc, show_pcs, rentar_pc_a_estudiant
 
 pcs_routes = Blueprint('pcs_routes', __name__)
 
+
 @pcs_routes.route('/computadoras', methods=['POST'])
 def add_pc():
     return create_pc()
+
 
 @pcs_routes.route('/computadoras', methods=['GET'])
 def get_pcs():
@@ -15,24 +17,27 @@ def get_pcs():
 def get_single_computer(id):
     return get_computer(id)
 
+
 @pcs_routes.route('/computadoras/<int:id>', methods=['DELETE'])
 def delete_pc(id):
     return eliminar_Pc(id)
+
 
 @pcs_routes.route('/computadoras/rentar', methods=['PUT'])
 def rentar_pc():
     return rentar_pc_a_estudiante()
 
+
 @pcs_routes.route('/computadoras/devolver', methods=['PUT'])
 def return_pc():
     return devolver_pc()
+
 
 @pcs_routes.route('/computadoras/<int:id>', methods=['PUT'])
 def modify_computer(id):
     return update_pc(id)
 
+
 @pcs_routes.route('/computadoras/buscar', methods=['GET'])
 def search_pc():
     return buscar_pc()
-
-
--- back/src/routes/tablets_router.py
+++ back/src/routes/tablets_router.py
@@ -1,34 +1,43 @@
 from flask import Blueprint
 from controllers.tabletsController import create_tablet, show_tablets, rentar_tablet_a_estudiante, devolver_tablet, eliminar_tablet, buscar_tablet, get_tablet, update_tablet
+
 tablets_routes = Blueprint('tablets_routes', __name__)
+
 
 @tablets_routes.route('/tablets', methods=['POST'])
 def add_pc():
     return create_tablet()
 
+
 @tablets_routes.route('/tablets', methods=['GET'])
 def get_pcs():
     return show_tablets()
+
 
 @tablets_routes.route('/tablets/<int:id>', methods=['GET'])
 def get_single_tablet(id):
     return get_tablet(id)
 
+
 @tablets_routes.route('/tablets/<int:id>', methods=['DELETE'])
 def delete_tablet(id):
     return eliminar_tablet(id)
+
 
 @tablets_routes.route('/tablets/rentar', methods=['PUT'])
 def rentar_pc():
     return rentar_tablet_a_estudiante()
 
+
 @tablets_routes.route('/tablets/devolver', methods=['PUT'])
 def return_pc():
     return devolver_tablet()
 
+
 @tablets_routes.route('/tablets/<int:id>', methods=['PUT'])
 def modify_tablet(id):
     return update_tablet(id)
+
 
 @tablets_routes.route('/tablets/buscar', methods=['GET'])
 def search_tablet():
